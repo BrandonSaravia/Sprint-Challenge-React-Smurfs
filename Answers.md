efbd302957d44011a9ff3ff77e876cc8d416653f
BrandonSaravia <BPSaravia@gmail.com>
2019-04-05 14:23:04 -0400

    completed the mvp

--- Answers.md
+++ Answers.md
@@ -1,5 +1,19 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
+
+ - client-side routing occurs when a user enters a web application, the request to the server is prevented, and it is rendered internally by the javascript that is loaded on the page. Server-side routing occurs when a user enters a web application, the request to the server is made, and it refreshes a readable page to the browser to render.
+
 1.  What does HTTP stand for?
+
+ - HyperText Transfer Protocol
+
 1.  What does CRUD stand for?
+
+ - Create, Read, Update, and Delete
+
 1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+
+ -  .then() and .Catch()
+
 1.  Mention three tools we can use to make AJAX requests
+
+ - Post, Get, Put, Delete 
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -4,50 +4,65 @@ class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      smurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
+      
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
+    
     // add code to create the smurf using the api
-
+    this.props.addNewSmurf(this.state.smurf)
     this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+      smurf: {
+        name: '',
+        age: '',
+        height: ''
+      }
+    })
+    console.log(this.state.smurf)
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    e.persist();
+    let value = e.target.value;
+    this.setState(prevState => ({
+       smurf: {
+         ...prevState.smurf,
+         [e.target.name]: value
+       }
+        
+  }))};
 
   render() {
+    console.log(this.state.smurf)
     return (
       <div className="SmurfForm">
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={this.state.smurf.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={this.state.smurf.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={this.state.smurf.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick={this.addSmurf}>Add to the village</button>
         </form>
       </div>
     );
